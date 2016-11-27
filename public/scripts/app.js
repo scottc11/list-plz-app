a7dba157616f241bd0cd58c184f09354f7ff2e9e
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-11-26 19:46:57 -0500

    added a service to interect with the data api and the UI.  Service is only using a mock folder at the moment to play with fake data.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,53 @@
 'use strict'
 
 angular.module('listPlz', [])
-  .controller('mainCtrl', function($scope) {
+  .controller('mainCtrl', function($scope, dataService) {
+
+
     $scope.helloWorld = function() {
       console.log("hello there! This is the hello world controller function in the main controller");
+    };
+
+    // GET THE WISH LIST DATA FROM api
+    dataService.getWishList(function(response) {
+      console.log(response.data);
+      $scope.wishList = response.data;
+    });
+
+    $scope.addItem = function() {
+      var item = {"name": "This is a new todo"};
+      $scope.wishList.push(item);
     }
+
+    $scope.deleteItem = function(item, $index) {
+      dataService.deleteItem(item);
+      $scope.wishList.splice($index, 1);
+    };
+
+    $scope.saveItem = function(item, $index) {
+      dataService.saveItem(item);
+    };
+
+  })
+  .service('dataService', function($http) {
+
+    // THIS IS WHERE THE SERVICE WILL CONNECT WITH DATA API
+    this.getWishList = function(callback) {
+      $http.get('mock/list.json').then(callback)
+    }
+
+    this.deleteItem = function(item) {
+      console.log("The" + item.name + " item has been deleted!");
+      // other logic
+    }
+
+    this.saveItem = function(item) {
+      console.log("The" + item.name + " item has been saved!");
+    }
+
   });
+
+// TODO: add strike-through when item.purchased is == true
+// TODO: create an ng-model="item.purchased" and attach
+// it to the <input type="checkbox"> html.  This will be used
+// mark a users item as 'purchased' by setting item.purchased to 'true'
